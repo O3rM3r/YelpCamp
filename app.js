39d9bcb2faaed05fe1d244cad2a171bf8635dcd5
Omer Mayer <omer.mayer@gmail.com>
2020-01-14 21:34:35 +0200

    add start script & server port

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, process.env.IP, function(){
-    console.log("YelpCamp server has started");
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Server Has Started!");
 });
